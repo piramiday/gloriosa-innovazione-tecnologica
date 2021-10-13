b0a2532132c1e03026deda97654f0b300d7094c0
piramiday <piramiday@users.noreply.github.com>
2021-10-13 18:16:48 +0200

    unipd.sh

--- mitd.sh
+++ mitd.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-echo "<rss version=\"2.0\"><channel><title>MITD</title><lastBuildDate>$(date)</lastBuildDate>"
 D="https://innovazione.gov.it"
-curl "$D/dipartimento/posizioni-lavorative/" \
-| grep -oP '<a[^>]*text-primary[^>]*>[^>]*</a>' \
+curl "$D/dipartimento/posizioni-lavorative/" -o pl.htm
+echo -n "<rss version=\"2.0\"><channel><title>MITD</title><lastBuildDate>"
+grep -o '[0-9]*\ [a-z]*\ 2021' pl.htm \
+| head -n1 \
+| tr -d '\n'
+echo "</lastBuildDate>"
+grep -oP '<a[^>]*text-primary[^>]*>[^>]*</a>' pl.htm \
 | sed "s|^.*href=\"|$D|;s|\"[^>]*>\ *|\t|;s|\ *<.*$||" \
 | while read -r L T; do
    echo "<item><title>$T</title><guid>$L</guid></item>"
